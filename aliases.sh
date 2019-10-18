9a3d2840aef57a16c7f49e0487d53bb7a2346510
thedeceptio <thedeceptio@gmail.com>
2019-10-18 17:35:06 +0530

    Basic git shortcuts

---
+++ aliases.sh
@@ -0,0 +1,15 @@
+#This file will have all the aliases
+
+#Git Shortcuts
+alias gs='git status'
+alias gpl='git pull origin'
+alias gps='git push origin'
+alias gc='git checkout'
+alias gd='git diff'
+
+# List the no. of lines of codes added by different programmers in the current project
+#acronym Work Done By User
+alias wdbu='bash ~/superScripts/list_contributions'
+
+#List the no. of commits added by different programmers in the current porject
+alias ccbu='git shortlog -sn --all'
